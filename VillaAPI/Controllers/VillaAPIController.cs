05f57ab27e7eff47cf4e370724a355263da773c9
k3rnel_panic <shotov1303@gmail.com>
2023-05-31 23:25:46 +0400

    Added some endpoints, etc..

---
+++ VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VillaAPI.Data;
+using VillaAPI.Models;
+using VillaAPI.Models.Dto;
+
+namespace VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(x=> x.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+    }
+}
---
+++ VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using VillaAPI.Models.Dto;
+
+namespace VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+        {
+            new VillaDTO { Id = 1, Name="Mountain View"},
+            new VillaDTO { Id = 2, Name="Sea View"}
+        };
+    }
+}
